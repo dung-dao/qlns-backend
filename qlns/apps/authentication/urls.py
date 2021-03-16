09ed94b8e638cd9d8335315d6b35db920cd37131
Dung Dao <daomanhdung@hotmail.com>
2021-03-16 22:19:31 +0700

    create manage role function

--- qlns/apps/authentication/urls.py
+++ qlns/apps/authentication/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
+from rest_framework.routers import DefaultRouter
+from .views import GroupView
+
+router = DefaultRouter()
+router.register(r'roles', GroupView, basename='role')
 
 urlpatterns = [
     path('token/', jwt_views.TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(),
          name='token_refresh'),
+    path('', include(router.urls))
 ]
--- qlns/apps/authentication/views.py
+++ qlns/apps/authentication/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from qlns.apps.authentication.serializers import GroupSerializer
+from django.contrib.auth.models import Group
 
-# Create your views here.
+
+class GroupView(ModelViewSet):
+    serializer_class = GroupSerializer
+    queryset = Group.objects.all()
+    # permission_classes = ()
