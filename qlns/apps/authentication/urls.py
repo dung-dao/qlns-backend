188f025744aca7d511b1e93d7997de8f4708e840
Dung Dao <daomanhdung@hotmail.com>
2021-03-16 22:32:00 +0700

    create route to view permissions

--- qlns/apps/authentication/urls.py
+++ qlns/apps/authentication/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
 from rest_framework.routers import DefaultRouter
-from .views import GroupView
+from .views import GroupView, PermissionView
 
 router = DefaultRouter()
 router.register(r'roles', GroupView, basename='role')
+router.register(r'permissions', PermissionView, basename='role')
 
 urlpatterns = [
     path('token/', jwt_views.TokenObtainPairView.as_view(),
--- qlns/apps/authentication/views.py
+++ qlns/apps/authentication/views.py
@@ -1,9 +1,16 @@
-from rest_framework.viewsets import ModelViewSet
-from qlns.apps.authentication.serializers import GroupSerializer
-from django.contrib.auth.models import Group
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from qlns.apps.authentication.serializers import GroupSerializer, PermissionSerializer
+from django.contrib.auth.models import Group, Permission
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 
 class GroupView(ModelViewSet):
+    permission_classes = (IsAdminUser,)
     serializer_class = GroupSerializer
     queryset = Group.objects.all()
-    # permission_classes = ()
+
+
+class PermissionView(ReadOnlyModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    queryset = Permission.objects.all()
+    serializer_class = PermissionSerializer
