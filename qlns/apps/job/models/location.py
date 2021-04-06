7157f9c9b7aeeeaee16df58e5f73c18cfb4d0900
Dung Dao <daomanhdung@hotmail.com>
2021-04-06 22:21:39 +0700

    update: make name fields unique

--- qlns/apps/job/models/location.py
+++ qlns/apps/job/models/location.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Location(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, unique=True)
     country = models.ForeignKey(to='core.Country', on_delete=models.PROTECT)
     province = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
--- qlns/apps/job/models/work_shift.py
+++ qlns/apps/job/models/work_shift.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class WorkShift(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     start_time = models.TimeField()
     end_time = models.TimeField()
