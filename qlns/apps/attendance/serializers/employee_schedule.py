438c5da2dcaa3e4594b884e3307a9bcabd2ffd7b
Dung Dao <daomanhdung@hotmail.com>
2021-04-28 17:03:09 +0700

    update employee schedule view

--- qlns/apps/attendance/serializers/employee_schedule.py
+++ qlns/apps/attendance/serializers/employee_schedule.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from qlns.apps.attendance.models import EmployeeSchedule, Schedule
+from qlns.apps.attendance.serializers import ScheduleSerializer
 from qlns.apps.core.models import Employee
 
 
@@ -9,5 +10,22 @@ class EmployeeScheduleSerializer(serializers.ModelSerializer):
         model = EmployeeSchedule
         fields = ('schedule', 'owner',)
 
-    schedule = serializers.SlugRelatedField('name', queryset=Schedule.objects.all())
+    schedule = ScheduleSerializer(read_only=True)
     owner = serializers.PrimaryKeyRelatedField(queryset=Employee.objects.all())
+
+    def create(self, validated_data):
+        modified_validated_data = validated_data
+        request = self.context['request']
+        schedule_name = request.data.get('schedule')
+        schedule = Schedule.objects.filter(name=schedule_name).first()
+        modified_validated_data['schedule'] = schedule
+
+        return super(EmployeeScheduleSerializer, self).create(modified_validated_data)
+
+    def update(self, instance, validated_data):
+        modified_validated_data = validated_data
+        request = self.context['request']
+        schedule_name = request.data.get('schedule')
+        schedule = Schedule.objects.filter(name=schedule_name).first()
+        modified_validated_data['schedule'] = schedule
+        return super(EmployeeScheduleSerializer, self).update(instance, modified_validated_data)
