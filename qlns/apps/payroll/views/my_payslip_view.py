147f2e757a3fe1d852eb4a02aa5e950c8d422e3e
Dung Dao <daomanhdung@hotmail.com>
2021-06-05 09:12:47 +0700

    fix payslip permission

--- qlns/apps/payroll/views/my_payslip_view.py
+++ qlns/apps/payroll/views/my_payslip_view.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 
-from qlns.apps.authentication.permissions import IsOwner
+from qlns.apps.authentication.permissions import RWPermissionOrIsOwner
 from qlns.apps.payroll.models import Payslip
 from qlns.apps.payroll.serializers.payslip_serializer import PayslipSerializer
 
@@ -8,7 +8,7 @@ from qlns.apps.payroll.serializers.payslip_serializer import PayslipSerializer
 class MyPayslipView(viewsets.ReadOnlyModelViewSet):
     serializer_class = PayslipSerializer
     queryset = Payslip.objects.all()
-    permission_classes = (IsOwner,)
+    permission_classes = (RWPermissionOrIsOwner,)
 
     def get_queryset(self):
         return self.queryset.filter(owner=self.kwargs.get('employee_pk'))
