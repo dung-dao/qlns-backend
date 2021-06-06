2312e83e54abd5ef683c7bf385caebb56bc8febc
Đào Mạnh Dũng <daomanhdung@hotmail.com>
2021-06-06 17:16:24 +0700

    Revert "Merge branch 'bug-fixes-and-improvements' into 'develop'"
    This reverts merge request !25

--- qlns/apps/job/admin.py
+++ qlns/apps/job/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 
-from qlns.apps.job import models
-
 # Register your models here.
-admin.site.register(models.EmploymentStatus)
-admin.site.register(models.JobTitle)
-admin.site.register(models.Location)
-admin.site.register(models.TerminationReason)
