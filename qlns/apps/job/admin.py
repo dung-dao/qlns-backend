cade831373f47d47f73203c70d94f2db46f2ae37
Dung Dao <daomanhdung@hotmail.com>
2021-06-06 15:26:10 +0700

    update admin site

--- qlns/apps/job/admin.py
+++ qlns/apps/job/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from qlns.apps.job import models
+
 # Register your models here.
+admin.site.register(models.EmploymentStatus)
+admin.site.register(models.JobTitle)
+admin.site.register(models.Location)
+admin.site.register(models.TerminationReason)
