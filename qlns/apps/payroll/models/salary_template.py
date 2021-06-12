c4f7d9392c84e7e09af795bc6305983d23e952a6
Dung Dao <daomanhdung@hotmail.com>
2021-06-12 14:30:22 +0700

    template modifiable & on create calculation

--- qlns/apps/payroll/models/salary_template.py
+++ qlns/apps/payroll/models/salary_template.py
@@ -1,6 +1,14 @@
 from django.db import models
 
+from qlns.apps.payroll import models as payroll_models
+
 
 class SalaryTemplate(models.Model):
     name = models.CharField(max_length=255, unique=True)
     is_default = models.BooleanField(default=False)
+
+    def is_modifiable(self):
+        has_confirmed_payroll = self.payrolls \
+            .filter(status=payroll_models.Payroll.Status.Confirmed) \
+            .exists()
+        return not has_confirmed_payroll
