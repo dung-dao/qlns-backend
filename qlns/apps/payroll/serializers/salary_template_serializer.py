31b4fcbc120a6ebb31a61505f709c07579a42b10
Dung Dao <daomanhdung@hotmail.com>
2021-06-15 22:09:50 +0700

    Better salary template validation

--- qlns/apps/payroll/serializers/salary_template_serializer.py
+++ qlns/apps/payroll/serializers/salary_template_serializer.py
@@ -1,6 +1,10 @@
+import formulas
+from formulas.errors import FormulaError
 from rest_framework import serializers
+from schedula import DispatcherError
 
-from qlns.apps.payroll.models import SalaryTemplate, SalaryTemplateField
+from qlns.apps.payroll.models import SalaryTemplate, SalaryTemplateField, SalarySystemField
+from qlns.apps.payroll.models.payroll_utils import PIT_VN
 from qlns.apps.payroll.serializers.salary_template_field_serializer import SalaryTemplateFieldSerializer
 
 
@@ -39,3 +43,59 @@ class SalaryTemplateSerializer(serializers.ModelSerializer):
             field.save()
 
         return instance
+
+    def validate(self, attrs):
+        functions = formulas.get_functions()
+        functions['PIT_VN'] = PIT_VN
+
+        fields = attrs.get('fields', [])
+        fields.sort(key=lambda e: e['index'])
+
+        system_fields = SalarySystemField.objects.all()
+
+        test_calculation_dict = {}
+        for sf in system_fields:
+            if sf.datatype == 'Text':
+                test_calculation_dict[sf.code_name] = 'a'
+            elif sf.datatype in ['Number', 'Currency', ]:
+                test_calculation_dict[sf.code_name] = 7
+
+        for index in range(len(fields)):
+            field = fields[index]
+            if field['type'] == 'System Field':
+                if field['datatype'] == 'Text':
+                    test_calculation_dict[field['code_name']] = 'a'
+                elif field['datatype'] in ['Number', 'Currency', ]:
+                    test_calculation_dict[field['code_name']] = 7
+            elif field['type'] == 'Formula':
+                try:
+                    formula = formulas.Parser().ast(f'={field["define"]}')[1].compile()
+
+                    # create context
+                    formula_context = {}
+                    inputs = formula.inputs
+                    for key in test_calculation_dict:
+                        if key.upper() in inputs:
+                            formula_context[key.upper()] = test_calculation_dict[key]
+
+                    # Try calculating
+                    ans = formula(**formula_context)
+
+                    # validate datatype
+                    if field['datatype'] == 'Text':
+                        ans = str(ans)
+                    elif field['datatype'] in ['Number', 'Currency', ]:
+                        ans = float(ans)
+
+                    test_calculation_dict[field["code_name"]] = ans
+
+                except (FormulaError,) as e:
+                    raise serializers.ValidationError({"fields": f'error at <{field["code_name"]}> : {str(e)}'})
+                except TypeError as e:
+                    raise serializers.ValidationError(
+                        {"fields": f'formula <{field["code_name"]}> {str(e)}'})
+                except ValueError as e:
+                    raise serializers.ValidationError(f'Invalid $datatype$ at <{field["code_name"]}> : {str(e)}')
+                except DispatcherError:
+                    raise serializers.ValidationError(f'$Undefined function$ at <{field["code_name"]}>')
+        return attrs
